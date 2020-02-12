df0f2b37994d6f5c2613f1cb3a36adbd4736135a
ilona <hellosidney17@gmail.com>
2020-02-12 12:21:34 +0200

    added hw-07

--- tasks/task-6.js
+++ tasks/task-6.js
@@ -3,7 +3,6 @@
 const input = document.querySelector('#validation-input');
 
 const validating = () => {
-    // input.classList.add('validation-input');
     if (input.value.length == input.getAttribute('data-length')){
         input.classList.add('valid');
         input.classList.remove('invalid');
